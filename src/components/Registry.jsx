0abaa61138ae572558a345ca594dbfa41e07c36d
Adarsh Prasad <adarshprasad005@gmail.com>
2026-06-29 23:13:32 +0530

    optimizd

--- src/components/Registry.jsx
+++ src/components/Registry.jsx
@@ -1,37 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import ThankYou from "./ThankYou"; // make sure this file exists
 
-const Venues = () => (
-  <section className="card venues">
-    <h3>Venue Locations</h3>
+const Registry = () => {
+  const [submitted, setSubmitted] = useState(false);
 
-    <div className="venue-block">
-      <h4>Wedding Ceremony</h4>
-      <p>SN Study Center, Ambalakavala</p>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.2869907911627!2d77.11329429999999!3d9.7417397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07ab7aff5348ef%3A0x7f17221d5cc11412!2sSN%20Study%20Center%20Ambalakavala!5e0!3m2!1sen!2sin!4v1782224936039!5m2!1sen!2sin"
-        width="100%"
-        height="350"
-        style={{ border: 0 }}
-        allowFullScreen=""
-        loading="lazy"
-        title="Wedding Venue Map"
-      ></iframe>
-    </div>
+  return (
+    <section id="registry" className="card">
+      <h3>Registry</h3>
+      {submitted ? (
+        <ThankYou onRedirect={() => setSubmitted(false)} />
+      ) : (
+        <>
+          <p>
+            Your blessings are our greatest gift. If you wish to share a token of love,
+            please leave your wishes below.
+          </p>
 
-    <div className="venue-block">
-      <h4>Reception</h4>
-      <p>BBC Auditorium, Prayar</p>
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.123456789!2d76.500000!3d9.300000!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07abcd123456%3A0xabcdef123456789!2sBBC%20Auditorium%20Prayar!5e0!3m2!1sen!2sin!4v1782224936039!5m2!1sen!2sin"
-        width="100%"
-        height="350"
-        style={{ border: 0 }}
-        allowFullScreen=""
-        loading="lazy"
-        title="Reception Venue Map"
-      ></iframe>
-    </div>
-  </section>
-);
+          {/* Embed Google Form */}
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSfTZrW_36kcw88s84lb4SDZU0gA5a9Ql-IdMOiREAXwMxO-lw/viewform?embedded=true"
+            width="640"
+            height="800"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0"
+            title="Blessings Form"
+          >
+            Loading…
+          </iframe>
 
-export default Venues;
+          {/* Button to confirm submission */}
+          <button
+            onClick={() => setSubmitted(true)}
+            className="rsvp-btn"
+            style={{ marginTop: "20px" }}
+          >
+            I’ve Submitted
+          </button>
+        </>
+      )}
+    </section>
+  );
+};
+
+export default Registry;
--- src/components/Venue.jsx
+++ src/components/Venue.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 
-const Venue = () => (
-  <section className="card">
-    <h3>Venue Location</h3>
-    <p>SN Study Center, Ambalakavala</p>
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.2869907911627!2d77.11329429999999!3d9.7417397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07ab7aff5348ef%3A0x7f17221d5cc11412!2sSN%20Study%20Center%20Ambalakavala!5e0!3m2!1sen!2sin!4v1782224936039!5m2!1sen!2sin"
-      width="100%"
-      height="350"
-      style={{ border: 0 }}
-      allowFullScreen=""
-      loading="lazy"
-      title="Venue Map"
-    ></iframe>
+const Venues = () => (
+  <section className="card venues">
+    <h3>Venue Locations</h3>
+
+    <div className="venue-block">
+      <h4>Wedding Ceremony</h4>
+      <p>SN Study Center, Ambalakavala</p>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.2869907911627!2d77.11329429999999!3d9.7417397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07ab7aff5348ef%3A0x7f17221d5cc11412!2sSN%20Study%20Center%20Ambalakavala!5e0!3m2!1sen!2sin!4v1782224936039!5m2!1sen!2sin"
+        width="100%"
+        height="350"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        title="Wedding Venue Map"
+      ></iframe>
+    </div>
+
+    <div className="venue-block">
+      <h4>Reception</h4>
+      <p>BBC Auditorium, Prayar</p>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3932.123456789!2d76.500000!3d9.300000!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b07abcd123456%3A0xabcdef123456789!2sBBC%20Auditorium%20Prayar!5e0!3m2!1sen!2sin!4v1782224936039!5m2!1sen!2sin"
+        width="100%"
+        height="350"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        title="Reception Venue Map"
+      ></iframe>
+    </div>
   </section>
 );
 
-export default Venue;
+export default Venues;
